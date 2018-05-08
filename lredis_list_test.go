21769916b54fc4c3364366d70da3a29ecd85ed0f
飞 <345364004@qq.com>
2018-05-08 10:17:51 +0800

    共两种获取连接池对象Pool的方法

--- lredis_list_test.go
+++ lredis_list_test.go
@@ -5,15 +5,41 @@ import (
 	"time"
 	"fmt"
 	"sync"
+	"container/list"
 )
 
-func Test_Pool_List(t *testing.T){
+var(
+	network = "tcp"
+	address = "182.92.158.94:6379"
+	pwd = "lifei"
+)
 
-	var(
-		network = "tcp"
-		address = "127.0.0.1:6379"
-		pwd = "lifei"
-	)
+func Test_PoolList_One(t *testing.T){
+
+	pool := &PoolList{
+		Conns:list.New(),
+		MinCaps:2,
+		MaxCaps:10,
+		IdleTimeout:10*time.Second,
+		Dial: func() (*Conn, error) {
+			return Dial(network,address,pwd)
+		},
+	}
+
+	var wg sync.WaitGroup
+
+	for i := 0;i < 30;i++{
+		wg.Add(1)
+		go testCmd(pool,i,&wg)
+	}
+
+	wg.Wait()
+
+	fmt.Println("finish......")
+}
+
+func Test_PoolList_Two(t *testing.T){
+
 	config := &PoolList_Config{
 		MinCaps:2,
 		MaxCaps:10,
@@ -35,8 +61,6 @@ func Test_Pool_List(t *testing.T){
 	}
 
 	wg.Wait()
-
-	//time.Sleep(2*time.Second)
 
 	fmt.Println("finish......")
 }
